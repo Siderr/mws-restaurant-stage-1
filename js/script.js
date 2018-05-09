afee99ba8686254143624753ea4aac26e46a6815
Tadas Bitkevicius <tadas.bit@gmail.com>
2018-05-09 15:19:44 +0300

    Added and registered service worker.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 /**
  * Created by tadas on 2018-05-02.
  */
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js', {scope: './'}).then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
